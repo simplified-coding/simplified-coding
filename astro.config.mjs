af07c9d15f020a2ce2b5397ff96e88828a955f7e
GalaxyGamingBoy <xrteach@gmail.com>
2023-06-28 13:14:11 +0000

    feat: Add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
-import mdx from "@astrojs/mdx"
+import mdx from "@astrojs/mdx";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    integrations: [mdx()]
+  output: 'server',
+  integrations: [mdx()],
+  adapter: vercel()
 });
