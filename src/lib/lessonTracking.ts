f252e830b299997a42488bd64a2fe751d951f894
Marios Mitsios <xrteach@gmail.com>
2023-12-30 14:55:49 +0200

    Update lessonTracking.ts

--- src/lib/lessonTracking.ts
+++ src/lib/lessonTracking.ts
@@ -1,24 +1,11 @@
 import {getEntry} from "astro:content";
 
 export const refreshLessons = async () => {
-  let data: any = {}
-
-  if (localStorage.getItem("tracking-lessons")) {
-    data = JSON.parse(localStorage.getItem("tracking-lessons") || "{}")
-  }
-
-  (await getEntry("lessons", "courses")).data.courses.map((c: { slug: string }) => {
-    if (!data.hasOwnProperty(c.slug)) {
-      data[c.slug] = []
-    }
+  let data = (await getEntry("lessons", "courses")).data.courses.map((c: { slug: string }) => {
+    c.slug
   })
 
-  console.log("[INFO] Lesson Cookies Refreshed")
-  localStorage.setItem("tracking-lessons", JSON.stringify(data))
-
-  if (localStorage.getItem("tracking-completed") == null) {
-    localStorage.setItem("tracking-completed", JSON.stringify("[]"))
-  }
+  localStorage.setItem("lessons", JSON.stringify(data))
 }
 
 export const addLesson = async () => {
@@ -28,7 +15,11 @@ export const addLesson = async () => {
   const item = localStorage.getItem(course)
   if (item) {
     const data: Array<string> = JSON.parse(item || "[]")
-    data.push(lesson || "")
+
+    if (data.indexOf(lesson || "") == -1) {
+      data.push(lesson || "")
+    }
+
     localStorage.setItem(course, JSON.stringify(data))
   } else {
     localStorage.setItem(course, JSON.stringify([lesson]))
