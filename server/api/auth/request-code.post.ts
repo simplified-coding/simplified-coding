47859da94f47f077a9bf3c16c95505d1673247a4
Marios <xrteach@gmail.com>
2026-08-26 14:28:52 +0300

    fix: request code error

--- server/api/auth/request-code.post.ts
+++ server/api/auth/request-code.post.ts
@@ -1,22 +1,22 @@
 import AuthService from "#server/utils/AuthService";
-import EmailService from "#server/utils/EmailService";
+import EmailService from "#server/utils/emailService";
 import * as z from "zod";
-import {type RequestCodeDto, requestCodeDto} from "#shared/utils/requestCodeDto";
+import { type RequestCodeDto, requestCodeDto } from "#shared/utils/requestCodeDto";
 
 export default defineEventHandler(async (e) => {
-    const res = await readValidatedBody(e, b => requestCodeDto.safeParse(b));
+  const res = await readValidatedBody(e, b => requestCodeDto.safeParse(b));
 
-    if (!res.success)
-        throw createError({
-            status: 422,
-            statusText: "Failed to parse request"
-        })
+  if (!res.success)
+    throw createError({
+      status: 422,
+      statusText: "Failed to parse request"
+    })
 
-    const data = res.data as RequestCodeDto;
-    const code = AuthService.requestCode(data.email);
+  const data = res.data as RequestCodeDto;
+  const code = AuthService.requestCode(data.email);
 
-    await EmailService.sendAuthenticationCode(data.email, code);
+  await EmailService.sendAuthenticationCode(data.email, code);
 
-    console.log(`${data.email}: ${code}`);
-    return {msg: `Sent code to ${data.email}!`}
+  console.log(`${data.email}: ${code}`);
+  return { msg: `Sent code to ${data.email}!` }
 })
